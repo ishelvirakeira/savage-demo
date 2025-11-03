dc233c975dc508ff6d620a664728b02cfb814fdb
Elvira Keira Ishimwe <97549159+ishelvirakeira@users.noreply.github.com>
2025-11-03 10:45:51 -0500

    server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var db, collection;
 
 const url = "mongodb+srv://ishelvirakeira_db_user:QjtwX7dDGqX9Ivk9@cluster0.vnt1rh6.mongodb.net/"+ dbName + "?retryWrites=true&w=majority";
 
-app.listen(3000, () => {
+app.listen(3800, () => {
     MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
         if(error) {
             throw error;
